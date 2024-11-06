d71a6d29a9e8c5597d15579b07026e7c3110030f
Jonathan AvilesCedeno <jonathan.aviles.c@gmail.com>
2024-11-06 15:55:36 +0100

    add example.py

---
+++ example.py
@@ -0,0 +1,14 @@
+"""
+Created on Wed Nov  6 15:52:43 2024
+
+@author: jaavilescedeno
+"""
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a + b  # do not change this line until prompted to do so.
+
+def test_add():
+    assert add(4,14)==18
