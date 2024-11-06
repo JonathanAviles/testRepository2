026b128beb4f15ef622a874693d9cac438d55e7a
Jonathan AvilesCedeno <jonathan.aviles.c@gmail.com>
2024-11-06 16:26:28 +0100

    final commit

--- test_example.py
+++ test_example.py
@@ -6,6 +6,10 @@ Created on Wed Nov  6 16:07:22 2024
 """
 
 from example import add
+from example import subtract
 
 def test_add():
     assert add(4,14)==18
+    
+def  test_subtract():
+    assert substract(5,1)==4
